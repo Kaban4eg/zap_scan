ec4008ac2e8a59f465e4e14e328d630cb6750b2c
Kaban4eg <66689503+Kaban4eg@users.noreply.github.com>
2020-12-08 18:52:59 +0300

    Update jsondiff.py

--- jsondiff.py
+++ jsondiff.py
@@ -3,13 +3,13 @@ from deepdiff import DeepDiff
 import json
 
 def main():
-    f1=sys.argv[1]
+    #f1=sys.argv[1]
     f2=sys.argv[2]
     #f1 = "D:\\zap_report\\ino.json"  # standart
     #f2 = "D:\\zap_report\\new.ino.json"  # new scan
-    with open(f1) as json_file:
-        data1 = json.load(json_file)
-
+    #with open(f1) as json_file:
+    #    data1 = json.load(json_file)
+    data1=sys.argv[1]
     with open(f2) as json_file:
         data2 = json.load(json_file)
     ddiff = DeepDiff(data1, data2, ignore_order=True)
