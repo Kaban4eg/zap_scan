abca68e3e6fad8f07a017ded14fe2881ae9cb569
Kaban4eg <66689503+Kaban4eg@users.noreply.github.com>
2020-12-08 20:38:51 +0300

    Update jsondiff.py

--- jsondiff.py
+++ jsondiff.py
@@ -3,18 +3,22 @@ from deepdiff import DeepDiff
 import json
 
 def main():
-    #f1=sys.argv[1]
+    f1=sys.argv[1]
     f2=sys.argv[2]
     #f1 = "D:\\zap_report\\ino.json"  # standart
     #f2 = "D:\\zap_report\\new.ino.json"  # new scan
-    #with open(f1) as json_file:
-    #    data1 = json.load(json_file)
-    data1=sys.argv[1]
-    with open(f2) as json_file:
+    with open(f1,encoding='utf8') as json_file:
+        data1 = json.load(json_file)
+        #data1 = json_file.read().replace('\\', '/')
+
+    with open(f2,encoding='utf8') as json_file:
         data2 = json.load(json_file)
+        #data2 = json_file.read().replace('\\', '/')
     ddiff = DeepDiff(data1, data2, ignore_order=True)
+    #print(ddiff)
     for inst in ddiff['iterable_item_added']:
         print(ddiff['iterable_item_added'][inst]['uri'] + " : " + ddiff['iterable_item_added'][inst]['method'])
 
 if __name__ == '__main__':
     main()
+
